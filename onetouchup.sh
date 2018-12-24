b9cf9cf1cf0b60a31a0ab6deec879a6045843639
aficustree <shaun@sjbaker.us>
2018-12-24 13:09:53 -0600

    autoupdater build

--- onetouchup.sh
+++ onetouchup.sh
@@ -1,9 +1,32 @@
 #!/bin/sh
+# run with env variable set to your PIN number
 
+export PIN=$PIN
 docker volume create homebridge
 docker volume create nx584plugins
-git clone https://github.com/aficustree/interlogixDockerizedHomebridge.git
-mv ./interlogixDockerizedHomebridge/homebridge/* /var/lib/docker/volumes/homebridge/_data
-
-git clone https://github.com/aficustree/nx584ADPlugin
-mv ./nx584ADPlugin/* /var/lib/docker/volumes/nx584plugins/_data
+rm -rf ./tmp
+git clone https://github.com/aficustree/interlogixDockerizedHomebridge.git tmp 
+if [ $? -eq 0 ]
+then
+  rm -rf ./interlogixDockerizedHomebridge
+  mv ./tmp ./interlogixDockerizedHomebridge
+  mv ./interlogixDockerizedHomebridge/onetouch.sh .
+  chmod 755 onetouch.sh
+  rm -rf /var/lib/docker/volumes/homebridge/_data/*
+  mv ./interlogixDockerizedHomebridge/homebridge/* /var/lib/docker/volumes/homebridge/_data
+else
+  echo "Couldn't pull interlogixDockerizedHomebridge, using existing"
+fi
+rm -rf ./tmp
+git clone https://github.com/aficustree/nx584ADPlugin tmp
+if [ $? -eq 0 ]
+then
+  rm -rf ./nx584ADPlugin
+  mv ./tmp ./nx584ADPlugin
+  rm -rf /var/lib/docker/volumes/nx584plugins/_data/*
+  mv ./nx584ADPlugin/* /var/lib/docker/volumes/nx584plugins/_data
+else
+  echo "Couldn't pull nx584ADPlugin, using existing"
+fi
+cat /var/lib/docker/volumes/homebridge/_data/config.json | jq --arg mypin "$PIN" '(.platforms[] | select (.platform == "alarmdecoder-platform").setPIN)=$mypin' | sponge /var/lib/docker/volumes/nx584plugins/_data/config.json 
+exit 0
